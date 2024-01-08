3432e07d2e3d43dc4072efca81ae96f0b03f1309
lucax011 <csd6924@gmail.com>
2024-01-08 15:12:21 -0300

    Finalizando projeto

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    static void Main()
+    {
+        // Testes
+        Smartphone iphone = new Iphonec("123456789", "iPhone X", "ABC123", 64);
+        iphone.InstalarAplicativo("WhatsApp");
+
+        Smartphone nokia = new Nokia("987654321", "Nokia 3310", "XYZ789", 8);
+        nokia.InstalarAplicativo("Snake");
+    }
+}
