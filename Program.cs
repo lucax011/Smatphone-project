05b9513bcdebdcc254137df64adf085a8c7008c3
lucax011 <csd6924@gmail.com>
2024-01-08 15:14:17 -0300

    finalizado

--- Program.cs
+++ Program.cs
@@ -8,8 +8,13 @@ class Program
         // Testes
         Smartphone iphone = new Iphonec("123456789", "iPhone X", "ABC123", 64);
         iphone.InstalarAplicativo("WhatsApp");
-
+        iphone.Ligar();
+        iphone.ReceberLigacao();
+       
         Smartphone nokia = new Nokia("987654321", "Nokia 3310", "XYZ789", 8);
         nokia.InstalarAplicativo("Snake");
+        nokia.Ligar();
+        nokia.ReceberLigacao();
+
     }
 }
